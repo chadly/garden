e828ac124ec8950b5e11b856c663467a85700f7d
Chad Lee <git@chadly.net>
2023-09-23 17:08:57 -0500

    🐛 fix: base URL

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "garden.chadly.net",
+    baseUrl: "www.chadly.net",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
