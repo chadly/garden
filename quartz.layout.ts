538fa59513b6aec97c45db2736e243a6f7156ce7
Chad Lee <git@chadly.net>
2023-09-24 18:53:13 -0500

    ✨ feat: remove explorer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -33,7 +33,6 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
   ],
   right: [
     Component.Graph(),
