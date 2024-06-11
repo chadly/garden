f44e1cce7d18166e1544d54c53e5911e0441bbe6
Chad Lee <git@chadly.net>
2024-06-11 14:03:04 -0500

    💬 text: tweak title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🧠🌳",
+    pageTitle: "🧠🌳 Chad's Mind Garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
